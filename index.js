762c5cd055d9a39070d13ee38adf159278eea2d3
lucaf1990 <117093377+lucaf1990@users.noreply.github.com>
2023-01-23 16:13:02 +0100

    sd

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ let tableToFill1 = document.getElementById("two");
 let tableToFill2 = document.getElementById("three");
 let button = document.getElementById("button");
 
-let NewUser = (name, surname, dateOfBirth) => {
+let NewUser = function (name, surname, dateOfBirth) {
   this.name = name;
   this.surname = surname;
   this.dateOfBirth = dateOfBirth;
 };
+let user1 = new NewUser();
+console.log(user1);
 
-button.onclick = () => {
-  tableToFill.innerHTML = name.value;
-  tableToFill1.innerHTML = surname.value;
-  tableToFill2.innerHTML = dateOfBirth.value;
+button.onclick = function () {
+  user1.name = name.value;
+  user1.surname = surname.value;
+  user1.dateOfBirth = dateOfBirth.value;
+  tableToFill.innerHTML = user1.name;
+  tableToFill1.innerHTML = user1.surname;
+  tableToFill2.innerHTML = user1.dateOfBirth;
 };
+console.log(user1);
